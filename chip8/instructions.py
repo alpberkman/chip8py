661e2e0c8d466d6ff04d0603c30c9308b75ac566
Alp Berkman <alpberkman@gmail.com>
2025-10-08 20:33:47 +0200

    Implemented more instruction and pretty printing

--- chip8/instructions.py
+++ chip8/instructions.py
@@ -1,4 +1,3 @@
-
 from functools import reduce
 
 
@@ -12,80 +11,190 @@ class Instr:
     id = None
     name = "BASE"
 
-    def __init__(self, opcode, **kwargs):
+    def __init__(self, opcode: int, **kwargs):
         self.opcode = opcode
 
     def eval(self, emu):
         raise OpcodeNotImplementedError(self)
-        return emu
-    
+
     def __call__(self, emu):
-        return self.eval(emu)
+        self.eval(emu)
+        return emu
 
     def __str__(self):
-        return f"{self.name}: {self.id}({hex(self.opcode)})"
-    
+        l = [f"{self.name+':':4}"]
+
+        if self.id is not None:
+            l.append(f"[{self.id}|{hex(self.opcode)}]")
+
+        for arg in ["x", "y", "n", "nn", "nnn"]:
+            if arg in vars(self):
+                l.append(f"{arg.upper()}: {self.__dict__[arg]}")
+
+        return " ".join(l)
+
+
 class Chain(Instr):
     id = None
     name = "CHN"
+    seperator = "  "
 
     def __init__(self, *instrs: Instr, **kwargs):
         super().__init__(None, **kwargs)
         self.instrs = instrs
 
     def eval(self, emu):
-        if not self.instrs: return emu
-        else: return reduce(lambda acc, instr: instr(acc), self.instrs, emu)
+        if self.instrs:
+            return reduce(lambda acc, instr: instr(acc), self.instrs, emu)
+
+    def __str__(self, depth: int = 0):
+        lines = [f"{self.seperator * depth}{self.name}:"]
+
+        for instr in self.instrs:
+            if isinstance(instr, Chain):
+                lines.append(instr.__str__(depth + 1))
+            else:
+                lines.append(f"{self.seperator * (depth + 1)}{instr}")
+
+        return "\n".join(lines)
+
+
+class Dud(Instr):
+    id = None
+    name = "DMY"
+
+    def __init__(
+        self, opcode: int, x: int, y: int, n: int, nn: int, nnn: int, **kwargs
+    ):
+        super().__init__(opcode, **kwargs)
+        self.x = x
+        self.y = y
+        self.n = n
+        self.nn = nn
+        self.nnn = nnn
+
+    def eval(self, emu):
+        pass
 
 
 class Ix00E0(Instr):
     id = "00E0"
     name = "CLS"
 
-    def __init__(self, opcode, **kwargs):
-        super().__init__(opcode, **kwargs)
-
     def eval(self, emu):
         emu.screen.clear()
-        return emu
 
 
 class Ix00EE(Instr):
     id = "00EE"
     name = "RET"
 
-    def __init__(self, opcode, **kwargs):
-        super().__init__(opcode, **kwargs)
-
     def eval(self, emu):
         emu.pc = emu.stack.pop()
-        return emu
 
 
 class Ix1NNN(Instr):
     id = "1NNN"
     name = "JP"
 
-    def __init__(self, opcode, nnn, **kwargs):
+    def __init__(self, opcode: int, nnn: int, **kwargs):
         super().__init__(opcode, **kwargs)
         self.nnn = nnn
 
     def eval(self, emu):
         emu.pc = self.nnn
-        return emu
 
 
 class Ix2NNN(Instr):
     id = "2NNN"
     name = "CALL"
 
-    def __init__(self, opcode, nnn, **kwargs):
+    def __init__(self, opcode: int, nnn: int, **kwargs):
         super().__init__(opcode, **kwargs)
         self.nnn = nnn
 
     def eval(self, emu):
         emu.stack.append(emu.pc)
         emu.pc = self.nnn
-        return emu
 
-#Instr.__subclasses__()[0].id
+
+class Ix3XNN(Instr):
+    id = "3XNN"
+    name = "SE"
+
+    def __init__(self, opcode: int, x: int, nn: int, **kwargs):
+        super().__init__(opcode, **kwargs)
+        self.x = x
+        self.nn = nn
+
+    def eval(self, emu):
+        if emu.v[self.x] == self.nn:
+            emu.next()
+
+
+class Ix4XNN(Instr):
+    id = "4XNN"
+    name = "SNE"
+
+    def __init__(self, opcode: int, x: int, nn: int, **kwargs):
+        super().__init__(opcode, **kwargs)
+        self.x = x
+        self.nn = nn
+
+    def eval(self, emu):
+        if emu.v[self.x] != self.nn:
+            emu.next()
+
+
+class Ix5XY0(Instr):
+    id = "5XY0"
+    name = "SE"
+
+    def __init__(self, opcode: int, x: int, y: int, **kwargs):
+        super().__init__(opcode, **kwargs)
+        self.x = x
+        self.y = y
+
+    def eval(self, emu):
+        if emu.v[self.x] == emu.v[self.y]:
+            emu.next()
+
+class Ix6XNN(Instr):
+    id = "6XNN"
+    name = "LD"
+
+    def __init__(self, opcode: int, x: int, nn: int, **kwargs):
+        super().__init__(opcode, **kwargs)
+        self.x = x
+        self.nn = nn
+
+    def eval(self, emu):
+        emu.v[self.x] = self.nn
+
+
+class Ix7XNN(Instr):
+    id = "7XNN"
+    name = "ADD"
+
+    def __init__(self, opcode: int, x: int, nn: int, **kwargs):
+        super().__init__(opcode, **kwargs)
+        self.x = x
+        self.nn = nn
+
+    def eval(self, emu):
+        emu.v[self.x] += self.nn
+        
+
+class Ix8XY0(Instr):
+    id = "8XY0"
+    name = "LD"
+
+    def __init__(self, opcode: int, x: int, y: int, **kwargs):
+        super().__init__(opcode, **kwargs)
+        self.x = x
+        self.y = y
+
+    def eval(self, emu):
+        emu.v[self.x] = emu.v[self.y]
+
+# Instr.__subclasses__()[0].id
